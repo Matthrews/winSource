0884c365fbb913ef7d6751b3a2a1b544b4ec502d
qq1788lover <a17868810687@gmail.com>
2022-04-04 00:07:46 +0800

    djfkhdsjkfhdkjfsd

--- winSource/settings.py
+++ winSource/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
-STATIC_ROOT = 'collectstatic'
+STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.joinpath('upload')
