19f8f0457d9d830e171c219176789bde640f278c
qq1788lover <a17868810687@gmail.com>
2022-05-05 23:09:39 +0800

    扩大

--- myapp/models.py
+++ myapp/models.py
@@ -6,9 +6,9 @@ class Product(models.Model):
     # item_detail_title, item_detail_href, item_detail_manufacturer_title,item_detail_addinfo
     # item_availablity_text, item_availablity_low_price, item_availablity_env, item_availablity_pdf, item_picture
 
-    PartNo = models.CharField(verbose_name="产品名称", max_length=32, db_column='item_detail_title',
+    PartNo = models.CharField(verbose_name="产品名称", max_length=255, db_column='item_detail_title',
                               unique=True)  # item_detail_title
-    Manufacture = models.CharField(verbose_name="制造商", max_length=128,
+    Manufacture = models.CharField(verbose_name="制造商", max_length=256,
                                    db_column='item_detail_manufacturer_title')  # item_detail_manufacturer_title
     Catalog = models.CharField(verbose_name="目录", max_length=128, default=None, blank=True, null=True)
     Description = models.CharField(verbose_name="详情", max_length=512,
@@ -17,8 +17,8 @@ class Product(models.Model):
     ProductionPDFLink = models.URLField(verbose_name="产品详情PDF链接", default=None, blank=True,
                                         db_column='item_availablity_pdf')  # item_availablity_pdf
     Availability = models.CharField(verbose_name="产品总数量", default=None, blank=True,
-                                    db_column='item_availablity_text', max_length=32)  # item_availablity_text
-    LowestPrice = models.CharField(verbose_name="最低价格", max_length=32, default=None, blank=True,
+                                    db_column='item_availablity_text', max_length=256)  # item_availablity_text
+    LowestPrice = models.CharField(verbose_name="最低价格", max_length=128, default=None, blank=True,
                                    db_column='item_availablity_low_price')  # item_availablity_low_price
     MOQ = models.IntegerField(verbose_name="最小订购数量", default=None, blank=True, null=True)
     DetailLink = models.URLField(verbose_name="商品详情链接", db_column='item_detail_href')  # item_detail_href
